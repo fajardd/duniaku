796224b63e55bbcaece546543e7cfce14be18c15
fjrdwc <fjrdwc@gmail.com>
2026-04-29 11:39:48 +0700

    fix: hosting firebase deploy

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTOWwqxnpmdBYSLQPwaWMFX8oDkbMiWVw",
-  authDomain: "clara-25042026.firebaseapp.com",
-  projectId: "clara-25042026",
-  storageBucket: "clara-25042026.firebasestorage.app",
-  messagingSenderId: "392737208426",
-  appId: "1:392737208426:web:2fec2cd7261e1f59007213",
-  measurementId: "G-RXD9VNTKFD",
+  apiKey: "AIzaSyBou0J-W9PdcLYSWhJxEMVd4y1NhsxTngk",
+  authDomain: "clara-claurita-salindri.firebaseapp.com",
+  projectId: "clara-claurita-salindri",
+  storageBucket: "clara-claurita-salindri.firebasestorage.app",
+  messagingSenderId: "1026801504331",
+  appId: "1:1026801504331:web:c12ff4841b912198787dce",
+  measurementId: "G-27VMS9SYE9",
 };
 
 const app = initializeApp(firebaseConfig);
